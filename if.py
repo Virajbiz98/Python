9c7f496f3f1ec8fba9f4659b50b4243368a422fe
Virajbiz98 <Virajbiz98@gmail.com>
2024-09-19 15:54:24 -0700

    Create if.py
    if and els simple examples

---
+++ if.py
@@ -0,0 +1,15 @@
+age = 18
+if age >= 18:
+    print("you have to vote for sl", 2024, "sl election on", 21, "th sunday.")
+----------------------------
+win = 67
+if win >= 67:
+    print("who is the next mad sl eater in siri lanka who can predict  it i ll give valuble aluth besamak for him whatever akd will be win with", 67.)
+  ---------------------------
+grade = 75
+if grade >= 75:
+    print("ela kollek!")
+    els:print("harak horek.")
+  ------------------
+
+        
