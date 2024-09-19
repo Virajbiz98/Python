c3f0b67a00561f53c205c091aefdc70ab9fb7dcf
Virajbiz98 <Virajbiz98@gmail.com>
2024-09-19 14:29:21 -0700

    Create variables.py
    variable examples

---
+++ variables.py
@@ -0,0 +1,23 @@
+salary=45000
+print("my salary" ,salary)
+---------------
+age=26
+print("my age is", age)
+-----------------
+f=25
+z=425
+t=f+z
+print(t)
+--------------------
+y=100
+x=200
+z=50
+p=x*y+z
+print(p)
+-------------------
+y=100
+x=200
+z=50
+p=x*y+z
+print(p)
+print("do you have", p)
