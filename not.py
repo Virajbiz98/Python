715bb800d5c8b8d6725e92a4026b0a58b4115c2e
Virajbiz98 <Virajbiz98@gmail.com>
2024-09-19 17:47:19 -0700

    Create not.py
    not examples

---
+++ not.py
@@ -0,0 +1,11 @@
+is_benten = True
+if not is_benten:
+    print("you are not a benten.")
+  --------------
+is_correct = True
+if not is_correct:
+    print("that is not correct.")
+  ----------
+is_doge = True
+if not is_doge:
+    print("is is not a doge.")
