b9fba7847973445ae7e20876521bb9b7f29f680a
Virajbiz98 <Virajbiz98@gmail.com>
2024-09-19 17:23:06 -0700

    Create or.py
    or examples

---
+++ or.py
@@ -0,0 +1,10 @@
+a = 10 
+b = -5 
+if a > 0 or b > 0:
+    print("at least one of a or b is positive.")
+  ---------------------
+t = 16 
+l = 9 
+if t < 10 or l > 5:
+    print("t or l is fals.")
+  ----------------
