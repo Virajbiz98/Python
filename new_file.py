7486712ab90b0cf4b5d00bc11db0a35ed8d22c5f
Virajbiz98 <Virajbiz98@gmail.com>
2024-09-20 10:08:05 -0700

    Update new_file.py

--- new_file.py
+++ new_file.py
@@ -1,3 +1,18 @@
 print("my age is 26")
 age=26
 print("my age is",age)
+-------------------
+e=30
+f=40
+if e or f >= 20:
+    print("s.")
+    els:print("f.")
+  ----------------
+fruits = ["bnana, mango, apple."]
+for fruit in fruits:
+    print(fruit)
+  ---------------------
+for number in range (8):
+    print(number)
+  ---------------
+
