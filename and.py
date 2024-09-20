971f2fd9ea487d993dd71c8be5cb755a63af0d7b
Virajbiz98 <Virajbiz98@gmail.com>
2024-09-19 17:10:27 -0700

    Create and.py
    and ex

---
+++ and.py
@@ -0,0 +1,20 @@
+A = 69
+B = 89
+if A >= 50 and B >= 50:
+    print("A th ekai B th ekai un okkoma yaluvo malli ranjan said")
+  -----------------------
+x = 40
+y = 35
+if x > 20 and y > 15:
+    print("both x and y are good for achchita hal garanna.")
+  -----------------------------
+kamal = 40
+amal = 30 
+if kamal > 35 and amal > 35:
+    print("kamal have s grade and amal have w grade.")
+  --------------------
+w = 50 
+q = 45
+if w < 60 and q < 60:
+    print("w and q have not enough marks for a grade.")
+  ---------------
