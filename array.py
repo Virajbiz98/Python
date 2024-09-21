8948753c679e6983d609e93c9cd39fb44fe47f57
Virajbiz98 <Virajbiz98@gmail.com>
2024-09-21 06:08:27 -0700

    Create array.py

---
+++ array.py
@@ -0,0 +1,11 @@
+my_array = [1,3,4,5,6]
+---
+student = ["sadini,tarindu,kamal"]
+-------
+for student in students:
+    print(student)
+---------
+def print_scores(scores):
+    for score in scores:
+        print(score)
+      ---------
